70e790369cea4c6c31149e0a8b82bc7ccbaf8fe6
strupphelm <tim.s.winter@gmail.com>
2023-02-28 11:33:26 +0100

    Added Optica - Josaa

---
+++ journals/optica_josaa.py
@@ -0,0 +1,44 @@
+from journals.journal_skeleton import Journal
+from journals.article import Article
+import requests
+from bs4 import BeautifulSoup
+
+
+class Josaa(Journal):
+    def __init__(self):
+        super().__init__()
+        self.base_url = "https://opg.optica.org/josaa/"
+
+    def get_newest_issues(self, n=1):
+        result = []
+        # in this case, all upcoming issues are the newest issue
+        issues = [self.base_url + "upcomingissue.cfm"]
+        # older issues have to be accessed differntly
+        issue_list_url = self.base_url + "browse.cfm"
+        issue_list = requests.get(issue_list_url)
+        soup = BeautifulSoup(issue_list.content, "html.parser")
+        # filter out links to upcoming issues
+        # (inefficient, since not all issues are needed, use generator in the future)
+        issues += [self.base_url + a['href'] for a in soup.find_all("a")
+                   if "Issue " in a.text and "upcoming" not in a['href']]
+        # get n latest issues
+        for i in range(n):
+            url = issues[i]
+            if not requests.get(url):
+                raise ConnectionError(f"Could not access issue {url}!")
+            else:
+                result += [url]
+        # return list of issue URLs
+        return result
+
+    def get_articles(self, issue_url):
+        articles_result = []
+        page = requests.get(issue_url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        articles = soup.find_all("div", class_="media-twbs-body")
+        for a in articles:
+            href = "https://opg.optica.org" + a.find("a")["href"]
+            title = a.find("a").text
+            authors = a.find("p", class_="article-authors").text
+            articles_result.append(Article(href, title, authors))
+        return articles_result
